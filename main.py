35467d979293a61d0911bb08e82c4abab16661f7
puhitaku <puhitaku@gmail.com>
2015-10-01 15:51:45 +0900

    Get rid of fxxking optparse :rage:

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import socket, requests, json, codecs
-from optparse import OptionParser
 from flask import Flask, request, jsonify
 from manager import AcquireManager, HeartBeatManager
 
@@ -31,42 +30,19 @@ def free():
     else:
         return jsonify({'mes': 'NG'}), 200
 
-def parse(usage):
-    p = OptionParser(usage)
-
-    p.add_option(
-        '-m', '--mode',
-        type='choice',
-        choices=['client', 'server'],
-        default='client',
-        metavar='MODE',
-        help='choose mode from "client" or "server"'
-    )
-
-    (opts, args) = p.parse_args()
-
-    return opts, args
-
 def main():
     print("""\
     Remote Sekisan Python-remix
     Takumi Sueda, 2014
     """)
-    
-    usage = 'usage: %prog [options] keyword'
-    (options, args) = parse(usage)
 
-    print('mode =', options.mode)
+    f = codecs.open('setting.json', 'r', 'utf-8')
+    setting = json.loads(f.read())
+    user = setting['user']
+    manager.set_user_list(user)
+    heart.set_ttl(setting['ttl'])
 
-    if options.mode == 'server':
-        f = codecs.open('setting.json', 'r', 'utf-8')
-        setting = json.loads(f.read())
-        user = setting['user']
-        manager.set_user_list(user)
-        heart.set_ttl(setting['ttl'])
-
-        app.run(host='0.0.0.0', debug=True)
-
+    app.run(host='0.0.0.0', debug=True)
 
 if __name__ == '__main__':
     main()
